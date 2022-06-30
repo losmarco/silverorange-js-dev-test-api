d72051975460fa044a712137d66e87afba0e73de
Marco Lai <laimgmarco@gmail.com>
2022-06-29 21:16:52 -0700

    Created  /repos API endpoint

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,31 @@
 import { Router, Request, Response } from 'express';
+//Had to use node-fetch v2
+const fetch = require('node-fetch');
+const fs = require('fs');
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
+  res.header('Content-Type', 'application/json');
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+
+  const url = 'https://api.github.com/users/silverorange/repos';
+  const response = await fetch(url);
+  const onlineRepo = await response.json();
+
+  fs.readFile('data/repos.json', 'utf8', (err: any, data: any) => {
+    if (err) {
+      return `Error: ${err}`;
+    }
+
+    const localFileRepo = JSON.parse(data);
+    const concatResult = [...onlineRepo, ...localFileRepo];
+    const result = concatResult.filter((e) => {
+      return e.fork === false;
+    });
+    res.json(result);
+  });
 });
